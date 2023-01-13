b93abebf4ff9ccf78d1aff1bf1b64114d3740dfd
Larissa Pretto <larissapretto009@gmail.com>
2023-01-13 16:17:49 -0300

    Delete Trabalho pgdb directory

--- Trabalho pgdb/BD/conexao.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conectado = mysqli_connect("localhost:3307", "root", "", "dbeleicoes");
-
-if (!$conectado) {
-	echo "Erro ao conectar";
-}
--- Trabalho pgdb/inicio.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('bd/conexao.php');
-include('verificar.php');
-?>
-
-<a class="dropdown-item" href="perfilUser.php">Perfil</a><br>
-<a class="dropdown-item" href="bd/cadastroResultado1.php">Cadastrar resultado</a><br>
-<a class="dropdown-item" href="sair.php">Sair</a>
