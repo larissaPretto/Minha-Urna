c905cd3764f23e009b234f5492942f1f6bcc1871
yurifmorais <yurimorais7113@gmail.com>
2023-01-15 22:59:31 -0300

    new tables

--- model/conexao.php
+++ model/conexao.php
@@ -1,5 +1,6 @@
 <?php
-$conectado = mysqli_connect("localhost:3307", "root", "", "dbeleicoes");
+// verificar porta do banco para nao dar erro
+$conectado = mysqli_connect("localhost:4306", "root", "", "dbeleicoes");
 
 if (!$conectado) {
 	echo "Erro ao conectar";
