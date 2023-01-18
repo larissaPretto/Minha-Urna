e4f52114e938439f79d47b665093861633e3718e
ezbecker <ezbecker@ufsm.com.br>
2023-01-18 11:24:32 -0300

    front

--- view/cadastroUser.php
+++ view/cadastroUser.php
@@ -2,99 +2,71 @@
 <html>
 
 <head lang="pt-br">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Criar conta | Minha Urna</title>  
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <style type="text/css">
-    .row {
-      display: -ms-flexbox;
-      /* IE10 */
-      display: flex;
-      -ms-flex-wrap: wrap;
-      /* IE10 */
-      flex-wrap: wrap;
-      padding: 44px 44px;
-    }
-  </style>
-  <title>Cadastro</title>
+  <link rel="stylesheet" href="cadastroUser.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
 
-  <center>
-    <br>
-    <h3>Cadastro</h3>
-    <br>
-    <div class="card">
-      <div class="card-body">
+  <img src="illustrations/loginSecurity.png">
+  <p class="title">Entrar</p>
+  <p class="subtitle">Digite as suas informações</p>
+  <div class="card">
+    <div class="container-fluid">
 
-        <form name="frmUsuario" action="../model/incluirCadastro.php" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
-          <div class="col-md-5 mb-3">
-            <label>Nome completo:</label>
-            <input type="text" name="nome" class="form-control" placeholder="Nome completo" required>
-            <div class="invalid-feedback">
-              Por favor, informe um nome válido.
-            </div>
-          </div>
+      <form name="frmUsuario" action="../model/incluirCadastro.php" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
+        <div class="nomeTextField">
+          <input type="text" name="nome" class="form-control" placeholder="Nome" required>
+        </div>
 
-          <div class="col-md-5 mb-3">
-            <label>E-mail:</label>
-            <input name="email" type="email" class="form-control" placeholder="E-mail" required>
-            <div class="invalid-feedback">
-              Por favor, informe um email válido.
-            </div>
-          </div>
+        <div class="emailTextField">
+          <input name="email" type="email" class="form-control" placeholder="E-mail" required>
+        </div>
 
-          <div class="col-md-5 mb-3">
-            <label>Senha:</label>
-            <input name="senha" type="password" class="form-control" aria-describedby="passwordHelpBlock" placeholder="Senha" required minlength="8" maxlength="15">
-            <small id="passwordHelpBlock" class="form-text text-muted">
-              Sua senha deve conter entre 8 a 15 caracteres.
-            </small>
-            <div class="invalid-feedback">
-              Por favor, informe uma senha válida.
-            </div>
-          </div>
-
-          <label>Foto do perfil:</label>
-          <div class="form-group" style="margin-left:390px">
-            <input name="fileUpload" type="file" class="form-control-file">
-          </div>
+        <div class="passwordTextField">
+          <input name="senha" type="password" class="form-control" aria-describedby="passwordHelpBlock" placeholder="Senha" required minlength="8" maxlength="15">
+          <small id="passwordHelpBlock" class="passwordHelp">
+            Sua senha deve conter de 8 a 15 caracteres.
+          </small>
+        </div>
       </div>
-
-
     </div>
-    </div>
-    <br>
-    <button type="submit" class="btn btn-primary">Cadastrar</button><br>
+  </div>
+  <br>
+  <button type="submit" class="entrar">Criar conta</button><br>
 
-    Caso seja um administrador, cadastre-se <a href="formPr.php">aqui</a>
-    <br>
-    <br>
-    <br>
+  <p class="helpText">Se já esteve aqui antes, faça o login!</p>
+  <a href="login.php">Fazer login</a>
+  <br>
 
-    </form>
-    </fieldset>
-    <script>
-      (function() {
-        'use strict';
-        window.addEventListener('load', function() {
+  </form>
+  </fieldset>
+  <script>
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
 
-          var forms = document.getElementsByClassName('needs-validation');
+        var forms = document.getElementsByClassName('needs-validation');
 
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
-  </center>
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+  </script>
+
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- view/login.php
+++ view/login.php
@@ -2,41 +2,36 @@
 <html>
 
 <head lang="pt-br">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Entrar | Minha Urna</title>  
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link href='https://fonts.googleapis.com/css?family=Monoton' rel='stylesheet'>
-
-  <title>Login</title>
-
+  <link rel="stylesheet" href="login.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
+  <img src="illustrations/loginSecurity.png">
+  <p class="title">Entrar</p>
+  <p class="subtitle">Digite as suas informações</p>
 
-  <center>
-    <br>
-    <br>
-
-    <div class="container-fluid">
-      <form action="../control/autenticar.php" method="POST">
-        <h1>Login</h1> <br>
-        <div class="col-md-4 mb-3">
-          <label for="exampleInputEmail1">Email</label>
-          <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" autofocus="">
-        </div>
-        <div class="col-md-4 mb-3">
-          <label for="exampleInputPassword1">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" name="senha">
-        </div>
-
-        Não tem uma conta?<a href="cadastroUser.php"> Cadastre-se</a>
-        <br>
-        <br>
-        <button type="submit" class="btn btn-primary">Entrar</button>
-      </form>
-    </div>
-  </center>
-
+  <div class="container-fluid">
+    <form action="../control/autenticar.php" method="POST">
+      <div class="emailTextField">
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" autofocus="">
+      </div>
+      <div class="passwordTextField">
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" name="senha">
+      </div>
+      
+      <button type="submit" class="entrar">Entrar</button>
+    </form>
+  </div>
+  
+  <p class="helpText">Se nunca esteve aqui antes, crie uma conta!</p>
+  <a href="cadastroUser.php"> Criar uma conta</a>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
