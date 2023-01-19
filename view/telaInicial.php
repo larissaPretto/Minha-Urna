3d677871956234ad9ba105f2cf6d0f169577d6d5
yurifmorais <yurimorais7113@gmail.com>
2023-01-19 10:48:31 -0300

    sql tela inicial funcionando

--- view/telaInicial.php
+++ view/telaInicial.php
@@ -39,10 +39,19 @@
         $consulta = mysqli_query($conectado, $sql);
         while ($registro = mysqli_fetch_array($consulta)) {
           $sqlDois = "SELECT * FROM candidatos where '$registro[idCandidato]' = id_candidato";
-          while ($registroDois = mysqli_fetch_array(mysqli_query($conectado, $sqlDois)))
-            echo "
+          $registroDois = mysqli_fetch_array(mysqli_query($conectado, $sqlDois));
+
+          $sqlPartidos = "SELECT * FROM partidos where '$registroDois[NR_PARTIDO]' = NR_PARTIDO";
+          $registroPartidos = mysqli_fetch_array(mysqli_query($conectado, $sqlPartidos));
+
+          $sqlCargo = "SELECT * FROM cargos where '$registroDois[CD_CARGO]' = CD_CARGO";
+          $registroCargo = mysqli_fetch_array(mysqli_query($conectado, $sqlCargo));
+
+          echo "
                             <tr>
                                 <td>$registroDois[NM_URNA_CANDIDATO]</td>
+                                <td>$registroPartidos[NM_PARTIDO]</td>
+                                <td>$registroCargo[NM_CARGO]</td>
                                 <td>$registro[votos]</td>
                             </tr>
                         ";
