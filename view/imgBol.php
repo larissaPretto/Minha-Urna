6f019b6a33a6744686b48b9ca14316bfaf00a0db
Larissa Pretto <larissapretto009@gmail.com>
2023-01-21 02:34:01 -0300

    agora deu

--- view/imgBol.php
+++ view/imgBol.php
@@ -5,10 +5,9 @@ $idBol = $_GET['idBol'];
 $sql = "SELECT * FROM boletim WHERE idBoletim=" . $idBol;
 $registro = mysqli_query($conectado, $sql);
 while ($registros = mysqli_fetch_array($registro)) {
-    echo '  <img src="../img/' . $registros['imgBoletim'] . '" alt=""style="position: absolute;
-              width: 216px;
-              height: 216px;
-              left: 64px;
-              top: -356px;
-              border-radius: 999px">';
+    $img = $registros['imgBoletim'];
+    echo $img;
+?>
+    <img src="../img/<?php echo $img ?>">
+<?php
 }
