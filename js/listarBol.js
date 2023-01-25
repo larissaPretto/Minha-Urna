9b82e9e6784630024562475118c59598822347b4
Larissa Pretto <larissapretto009@gmail.com>
2023-01-24 22:40:47 -0300

    ssa

--- js/listarBol.js
+++ js/listarBol.js
@@ -17,9 +17,10 @@ selectUF.onchange = () => {
 
 selectMuni.onchange = () => {
     let resul = document.getElementById('resul');
+    var cand = document.getElementById('res').getAttribute('value');
     let valor1 = selectMuni.value;
     let valor2 = selectUF.value;
-        fetch("../control/listarBol.php?selectMuni=" + valor1 + "&selectUF="+ valor2)
+        fetch("../control/listarBol.php?selectMuni=" + valor1 + "&selectUF="+ valor2 + "&idCand=" + cand)
             .then(response => 
             {
                 return response.text();
